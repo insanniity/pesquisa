82696e2660552baf8d1e7d90cdd3d62cb36cda13
insannity <32269793+insanniity@users.noreply.github.com>
2021-06-07 19:49:52 -0300

    Page Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from "./components/header";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 }
